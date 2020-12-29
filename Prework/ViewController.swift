2478500b9067b199df1e29c7234c43a7e945d050
zhong zheng <zz695920754@gmail.com>
2020-12-29 16:57:16 -0600

    complete navie version of prework

--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    
+    @IBOutlet weak var total: UILabel!
+    
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    
+    @IBAction func CalculateTip(_ sender: Any) {
+        
+        
+        let tipPercentages = [0.15, 0.18, 0.2]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        total.text = String(format: "$%.2f", tip + bill)
+        
+        // this is comment
+    }
+    
 
 
 }
