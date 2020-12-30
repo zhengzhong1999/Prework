77a48ffb6fa96699a370407377198b9a2598d20e
zhong zheng <zz695920754@gmail.com>
2020-12-30 11:44:56 -0600

    add setting pages and pass data using segue

---
+++ Prework/SettingViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SettingViewController.swift
+//  Prework
+//
+//  Created by zhong zheng on 12/29/20.
+//  Copyright © 2020 ZHONG ZHENG. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tip1: UITextField!
+    
+    @IBOutlet weak var tip2: UITextField!
+    
+    @IBOutlet weak var tip3: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+   
+    @IBAction func buttonPressed(_ sender: UIButton) {
+
+        let VC = self.navigationController?.viewControllers[0] as? ViewController
+        VC?.tip1 = tip1.text ?? "0"
+        VC?.tip2 = tip2.text ?? "0"
+        VC?.tip3 = tip3.text ?? "0"
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+
+
+}
--- Prework/ViewController.swift
+++ Prework/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var tip1 = "15"
+    var tip2 = "18"
+    var tip3 = "20"
+    
     @IBOutlet weak var billField: UITextField!
     
     
@@ -21,10 +25,40 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBOutlet weak var size: UILabel!
+    
+    
+    @IBOutlet weak var averagePay: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tipLabel.text = "$0.00"
+        total.text = "$0.00"
+        size.text = "0"
+        averagePay.text = "$0.00"
+        tipControl.setTitle(String(tip1)+"%", forSegmentAt: 0)
+        tipControl.setTitle(String(tip2)+"%", forSegmentAt: 1)
+        tipControl.setTitle(String(tip3)+"%", forSegmentAt: 2)
+        
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("hello")
+        super.viewWillAppear(animated)
+        tipLabel.text = "$0.00"
+        total.text = "$0.00"
+        size.text = "0"
+        averagePay.text = "$0.00"
+        tipControl.setTitle(String(tip1)+"%", forSegmentAt: 0)
+        tipControl.setTitle(String(tip2)+"%", forSegmentAt: 1)
+        tipControl.setTitle(String(tip3)+"%", forSegmentAt: 2)
+    }
+    
     
     
     @IBAction func onTap(_ sender: Any) {
@@ -36,6 +70,30 @@ class ViewController: UIViewController {
     @IBAction func CalculateTip(_ sender: Any) {
         
         
+        let a = Double(tip1)
+        let b = Double(tip2)
+        let c = Double(tip3)
+        let tipPercentages = [a,b,c]
+        
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * (tipPercentages[tipControl.selectedSegmentIndex] ?? 0) / 100
+        
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        total.text = String(format: "$%.2f", tip + bill)
+        
+        let n = size.text
+        
+         averagePay.text = String(format: "$%.2f", (tip+bill)/Double((Int(n ?? "1") ?? 1)))
+        
+    }
+    
+    
+    @IBAction func ChangeSize(_ sender: UIStepper) {
+        
+        size.text = Int(sender.value).description
         let tipPercentages = [0.15, 0.18, 0.2]
         
         let bill = Double(billField.text!) ?? 0
@@ -47,7 +105,14 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tip)
         total.text = String(format: "$%.2f", tip + bill)
         
-        // this is comment
+        let n = size.text
+        averagePay.text = String(format: "$%.2f", (tip+bill)/Double((Int(n ?? "1") ?? 1)))
+    }
+    
+    
+    
+    @IBAction func goToSetting(_ sender: Any) {
+        performSegue(withIdentifier: "setting", sender: self)
     }
     
 
